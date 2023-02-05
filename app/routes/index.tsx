c31caf42e7f9a44498814005f238e71223685566
Richard Miller <richardmmill@gmail.com>
2023-02-05 12:54:22 -0500

    add links and route scoped css

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,12 @@
+import type { LinksFunction } from "@remix-run/node";
+
+import stylesUrl from "~/styles/index.css";
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: stylesUrl }];
+};
+
+
 export default function Home() {
     return (
         <div>Hello World, Home Route</div>
