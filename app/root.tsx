498b16f05d808145d0f703e87c73388fe414856c
Richard Miller <richardmmill@gmail.com>
2023-02-06 20:05:54 -0500

    media queries route scoped

--- app/root.tsx
+++ app/root.tsx
@@ -1,10 +1,27 @@
 import { LiveReload, Outlet, Links } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
-import stylesUrl from "~/styles/global.css";
+import globalStylesUrl from "~/styles/global.css";
+import globalMediumStylesUrl from "~/styles/global-medium.css";
+import globalLargeStylesUrl from "~/styles/global-large.css";
 
 
 export const links: LinksFunction = () => {
-  return [{ rel: "stylesheet", href: stylesUrl }];
+  return [
+    {
+      rel: "stylesheet",
+      href: globalStylesUrl,
+    },
+    {
+      rel: "stylesheet",
+      href: globalMediumStylesUrl,
+      media: "print, (min-width: 640px)",
+    },
+    {
+      rel: "stylesheet",
+      href: globalLargeStylesUrl,
+      media: "screen and (min-width: 1024px)",
+    },
+  ];
 };
 
 export default function App() {
